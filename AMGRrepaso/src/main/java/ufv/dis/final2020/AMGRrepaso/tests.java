e602792938f8a62daa7faf6284d9633be5dcbb0b
anekom <andreeiitaa100ggmail.com>
2020-06-21 19:05:01 +0200

    prueba

---
+++ AMGRrepaso/src/main/java/ufv/dis/final2020/AMGRrepaso/tests.java
@@ -0,0 +1,14 @@
+package ufv.dis.final2020.AMGRrepaso;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class tests {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
